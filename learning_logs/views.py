2a69eca5895c7ca2d4c173b2de7fd07d61ef96f1
kaungkhantthar7777 <kaungkhantthar@rezeve.com>
2024-03-15 19:23:26 +0630

    added: create and edit entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm,EntryForm
 
 
 # Create your views here.
@@ -40,3 +40,40 @@ def new_topic(request):
     
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry"""
+    topic_data = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        # No date submitted, createa blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry_data = form.save(commit=False)
+            new_entry_data.topic = topic_data
+            new_entry_data.save()
+            return redirect('learning_logs:topic', topic_id= topic_id)
+    # Display a blank form or invalid form
+    context = {'topic': topic_data, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit on exiting entry"""
+    entry_data = Entry.objects.get(id=entry_id)
+    topic_data = entry_data.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with current entry
+        form = EntryForm(instance=entry_data)
+    else:
+        # POST form submitted; process data
+        form = EntryForm(instance=entry_data, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic_data.id)
+    context = {'entry':entry_data, 'topic': topic_data, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
