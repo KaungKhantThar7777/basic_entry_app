dc922ef3c5a4de969426d1709c68b5784911637e
kaungkhantthar7777 <kaungkhantthar@rezeve.com>
2024-03-16 05:41:26 +0630

    added: register feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 """Define URL patterns for accounts"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     # Include default app urls
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name="register")
 ]
